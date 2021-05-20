5c2c295793b84bf7248337fd7fa7876d15caea52
Hugh Lau <hughlau@berkeley.edu>
2021-05-19 18:57:04 -0700

    1

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const bodyParser = require('body-parser')
 const Joi = require('joi')
 
 
-
-
-
 // // EJS //
 // app.use('/public', express.static(path.join(__dirname, 'static')));
 // app.set('view engine', 'ejs');
@@ -61,34 +58,34 @@ const Joi = require('joi')
 
 
 
-// app.use('/public', express.static(path.join(__dirname, 'static')));
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
+app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'static', 'index.html'))
-// }).listen(3000)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'))
+}).listen(3000)
 
-// app.post('/', (req, res) => {
-//     console.log(req.body)
+app.post('/', (req, res) => {
+    console.log(req.body)
 
-//     const schema = Joi.object().keys({
-//         email: Joi.string().trim().email().required(),
-//         password: Joi.string().min(5).max(10).required()
-//     });
+    const schema = Joi.object().keys({
+        email: Joi.string().trim().email().required(),
+        password: Joi.string().min(5).max(10).required()
+    });
 
-//     Joi.validate(req.body, schema, (err, result) => {
-//         if (err) {
-//             console.log(err);
-//             res.send('an error has occured');
-//         } else {
-//             res.send('successfully posted data')
-//         }
-//         console.log(result);
-//     });
-//     // res.json({ success: true })
-//     res.send('succesfully posted data')
-// })
+    Joi.validate(req.body, schema, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.send('an error has occured');
+        } else {
+            res.send('successfully posted data')
+        }
+        console.log(result);
+    });
+    // res.json({ success: true })
+    res.send('succesfully posted data')
+})
 
 
 // const express = require('express')
